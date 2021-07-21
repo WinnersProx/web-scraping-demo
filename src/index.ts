da3b96c6172dc08f81533bc7cbd59128c8e904b3
WinnersProx <bihames4vainqueur@gmail.com>
2021-07-21 02:03:16 +0200

    ch(promise): refactor with the use of await

--- src/index.ts
+++ src/index.ts
@@ -8,30 +8,28 @@ const hackerNewsUrl = 'https://news.ycombinator.com/';
  * @returns Promise<Array<any>>
  */
 const scrapHackerNewsPosts = async () => {
-  return axios
-    .get(hackerNewsUrl)
-    .then((res) => {
-      const $ = cheerio.load(res.data);
-      const itemsList = $('.itemlist tbody').children();
-
-      return Array.from(itemsList).map((item) => {
-        const title = $(item).children('.title');
-        const titleLink = title.find('a.storylink');
-        const timeELement = title.parent().next().find('span.age');
-
-        const url = titleLink.attr('href') || null;
-
-        return {
-          title: titleLink.text(),
-          url,
-          createdAt: timeELement.attr('title'),
-        };
-      });
-    })
-    .catch((error) => {
+    const html = await axios.get(hackerNewsUrl).catch((error) => {
       console.error(error);
       process.exit(1);
     });
+
+    const $ = cheerio.load(html.data);
+    const itemsList = $('.itemlist tbody').children();
+
+    return Array.from(itemsList).map((item) => {
+      const title = $(item).children('.title');
+      const titleLink = title.find('a.storylink');
+      const timeELement = title.parent().next().find('span.age');
+
+      const url = titleLink.attr('href') || null;
+
+      return {
+        title: titleLink.text(),
+        url,
+        createdAt: timeELement.attr('title'),
+      };
+    });
+    
 };
 
 scrapHackerNewsPosts().then((posts) => {
