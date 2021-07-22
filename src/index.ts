9b3930f8b165f528645e677c877c3bbd514aab77
WinnersProx <bihames4vainqueur@gmail.com>
2021-07-22 08:53:30 +0200

    feat(pupetter): use pupetter automate authentication task

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,63 @@
-import cheerio from 'cheerio';
-import axios from 'axios';
+import puppeteer from 'puppeteer';
+
+import dotenv from 'dotenv';
+
+dotenv.config({});
 
 const hackerNewsUrl = 'https://news.ycombinator.com/';
 
-/**
- * Hacker news Scrapper
- * @returns Promise<Array<any>>
- */
-const scrapHackerNewsPosts = async () => {
-    const html = await axios.get(hackerNewsUrl).catch((error) => {
-      console.error(error);
-      process.exit(1);
-    });
 
-    const $ = cheerio.load(html.data);
-    const itemsList = $('.itemlist tbody').children();
+const getHNFeed = async () => {
+  const { HN_USERNAME, HN_PWD } = process.env || {};
 
-    return Array.from(itemsList).map((item) => {
-      const title = $(item).children('.title');
-      const titleLink = title.find('a.storylink');
-      const timeELement = title.parent().next().find('span.age');
+  const browser = await puppeteer.launch({ headless: true });
+  const page = await browser.newPage();
 
-      const url = titleLink.attr('href') || null;
+  await page.goto(`${hackerNewsUrl}login?goto=news`);
 
-      return {
-        title: titleLink.text(),
-        url,
-        createdAt: timeELement.attr('title'),
-      };
-    });
-    
+  await page.type('[name=acct]', HN_USERNAME);
+  await page.type('[name=pw]', HN_PWD);
+
+  await page.click('[value=login]');
+
+  const formatted = await page.evaluate(() => {
+    // access logged in user via document.querySelector('#me').innerHTML;
+    let itemsList = document.querySelector('.itemlist tbody').children;
+
+    return Array.from(itemsList)
+      .map((item) => {
+        const title = item.querySelector('td.title:not([valign="top"]');
+        const titleLink = title?.querySelector('a.storylink');
+
+        const timeELement =
+          title?.parentElement?.nextElementSibling?.querySelector('span.age');
+
+        const url = titleLink?.getAttribute('href') || null;
+
+        return {
+          title: titleLink?.textContent,
+          url,
+          createdAt: timeELement?.getAttribute('title'),
+        };
+      })
+      .filter((item) => item.title && item.url);
+  });
+
+  browser.close();
+
+  return formatted;
 };
 
-scrapHackerNewsPosts().then((posts) => {
-  console.log('posts', posts);
-});
+// must get the csrf and some other required cookies in case they are required
+const run = async () => {
+  await getHNFeed()
+    .then((data) => {
+      console.log('data', data);
+    })
+    .catch((error) => {
+      console.error('error occured', error);
+      process.exit(1);
+    });
+};
+
+run();
